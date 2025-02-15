0b3bd5dff48e864ba36cfd4880f283cb5b43cbe8
Batucan2601 <tosyalibatuhan@gmail.com>
2025-02-15 19:05:24 +0300

    Changed the hierarchy of menubar a bit.  What i've done can be clerly understood if GUIMenuBar::GUIMenuBar() function is checked.

--- include/GUI/GUIMenuBar.h
+++ include/GUI/GUIMenuBar.h
@@ -4,34 +4,39 @@
 #include <string>
 #include <memory>
 #include <functional>
+#include <map>
 
 class GUIBaseMenu
 {
 public:
 	virtual ~GUIBaseMenu() = default;
 	virtual void display() = 0;
+	std::map<std::string, std::unique_ptr<GUIBaseMenu>> items;
+	bool is_menu;
+	std::string title;
 };
 class GUIBarItem : public GUIBaseMenu
 {
 public:
-	std::string title;
 	std::function<void()> action;
-	GUIBarItem(const std::string &title, std::function<void()> action = nullptr)
-		: title(title), action(action) {}
+	GUIBarItem(const std::string& title, std::function<void()> action = nullptr);
 	void display() override;
 };
 class GUIMenu : public GUIBaseMenu
 {
 public:
+	GUIMenu(const std::string& title = "") : title(title)
+	{
+		is_menu = true; // this is a menu
+	}
 	std::string title;
-	std::vector<std::unique_ptr<GUIBaseMenu>> items;
 	void display() override;
 };
 class GUIMenuBar
 {
 public:
 	GUIMenuBar();
-	std::vector<GUIMenu> menuList;
+	std::map< std::string,std::unique_ptr<GUIBaseMenu>> menuList;
 	void Draw();
 };
 extern GUIMenuBar guiMenuBar;
--- src/GUI/GUIMenuBar.cpp
+++ src/GUI/GUIMenuBar.cpp
@@ -1,29 +1,93 @@
 #include "GUI/GUIMenuBar.h"
 #include <imgui.h>
+#include <sstream>
 
 GUIMenuBar guiMenuBar;
 
+static void createMenuItem(std::string name, bool is_menu , std::function<void()> action)
+{
+	if( name.find("/") == std::string::npos ) // at root
+	{
+		auto menu = std::make_unique<GUIMenu>(); 
+		menu->title = name;
+		guiMenuBar.menuList[name] = std::move(menu);
+		return; 
+	}
+	//else
+	std::vector<std::string> tokens;
+	std::stringstream ss(name);
+	std::string token;
+	// Split string by '/' delimiter.
+	while (std::getline(ss, token, '/')) {
+		tokens.push_back(token);
+	}
+	std::map<std::string, std::unique_ptr<GUIBaseMenu>>* currentMap = &guiMenuBar.menuList;
+	// Iterate through tokens.
+	// For each token except the last, ensure a GUIMenu exists.
+	for (size_t i = 0; i < tokens.size(); ++i) {
+		const std::string& name = tokens[i];
+		// Last token: create the final item.
+		if (i == tokens.size() - 1) {
+			// If an item with this name does not already exist, create it.
+			if (currentMap->find(name) == currentMap->end()) {
+				if (!is_menu) {
+					// Create a GUIBarItem.
+					auto barItem = std::make_unique<GUIBarItem>(name);
+					barItem->action = action;
+					(*currentMap)[name] = std::move(barItem);
+				}
+				else {
+					// Create a GUIMenu.
+					(*currentMap)[name] = std::make_unique<GUIMenu>(name);
+				}
+			}
+			else {
+				// Optionally update the existing item if needed.
+				// For this example, we do nothing.
+			}
+		}
+		else {
+			// For intermediate tokens, we need to ensure there's a GUIMenu.
+			if (currentMap->find(name) == currentMap->end()) {
+				// Create a new submenu (GUIMenu) if it doesn't exist.
+				(*currentMap)[name] = std::make_unique<GUIMenu>(name);
+			}
+			// Move the current map pointer to the items of the submenu.
+			GUIBaseMenu* base = (*currentMap)[name].get();
+			// Make sure that base is actually a menu (it should be).
+			currentMap = &(base->items);
+		}
+	}
+}
+static void createBarItem(std::string name)
+{
+
+}
+GUIBarItem::GUIBarItem(const std::string& title, std::function<void()> action)
+{
+	this->title = title;
+	this->action = action;
+	this->is_menu = false; 
+}
 GUIMenuBar::GUIMenuBar()
 {
-	GUIMenu fileMenu;
-	fileMenu.title = "File";
-
-	auto barItem = std::make_unique<GUIBarItem>("Open", []()
-												{
-		//std::cout << "Open Clicked!" << std::endl;
-		; });
-
-	fileMenu.items.push_back(std::move(barItem)); // FIX: Move unique_ptr
-
-	this->menuList.push_back(std::move(fileMenu));
+	createMenuItem("File" , true , nullptr);
+	createMenuItem("File/Open" , false , []()
+		{
+	; });
+	createMenuItem("File/Exit", false, []()
+		{
+			exit(1);
+	; });
+	
 }
 void GUIMenu::display()
 {
 	if (ImGui::BeginMenu(this->title.c_str()))
 	{
-		for (size_t i = 0; i < this->items.size(); i++)
+		for (auto& item : this->items) 
 		{
-			this->items[i]->display();
+			item.second->display();
 		}
 		ImGui::EndMenu();
 	}
@@ -32,16 +96,16 @@ void GUIBarItem::display()
 {
 	if (ImGui::MenuItem(this->title.c_str()))
 	{
-		// call func
+		this->action();
 	}
 }
 void GUIMenuBar::Draw()
 {
 	if (ImGui::BeginMainMenuBar())
 	{
-		for (size_t i = 0; i < this->menuList.size(); i++)
+		for (auto& item : this->menuList)
 		{
-			this->menuList[i].display();
+			item.second->display();
 		}
 		ImGui::EndMainMenuBar();
 	}
