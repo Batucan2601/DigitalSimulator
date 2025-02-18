14e03e38e1adcc383cee9c609bfda24874cb5522
Batucan2601 <tosyalibatuhan@gmail.com>
2025-02-18 12:25:11 +0300

    docking enabled.

--- src/GUI/GUIManager.cpp
+++ src/GUI/GUIManager.cpp
@@ -11,11 +11,37 @@ void GUIManager::Init()
     rlImGuiSetup(true); // Initialize ImGui
     GUIStyle_init();    // Set up custom ImGui styles
 }
+static void draw_parent_screen()
+{
+    // Get IO for display size
+    ImGuiIO& io = ImGui::GetIO();
+    // Determine your menubar height (e.g., using ImGui's frame height)
+    float menuBarHeight = ImGui::GetFrameHeight();
 
+    // Offset the dockspace window to start after the menubar
+    ImGui::SetNextWindowPos(ImVec2(0, menuBarHeight));
+    ImGui::SetNextWindowSize(ImVec2(io.DisplaySize.x, io.DisplaySize.y - menuBarHeight));
+
+    // Configure window flags for an invisible window
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse |
+        ImGuiWindowFlags_NoBackground |
+        ImGuiWindowFlags_NoBringToFrontOnFocus;
+
+    ImGui::Begin("InvisibleDockSpace", nullptr, window_flags);
+
+    // Create the dockspace (adjust flags as needed)
+    ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
+
+    ImGui::End();
+}
 void GUIManager::Draw()
 {
     rlImGuiBegin();
-
+    draw_parent_screen();
     // Draw individual components
     // GUIMenuBar::Draw();
     guiMenuBar.Draw();
