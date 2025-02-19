88000ee464df3752f699d3dc4efe59152e846f0a
Batucan2601 <tosyalibatuhan@gmail.com>
2025-02-19 15:26:15 +0300

    wtf missed this

--- include/JsonSerializer.h
+++ include/JsonSerializer.h
@@ -2,7 +2,7 @@
 #define JSONSERIALIZER_H
 #include "../Libraries/json.hpp"
 #include <string>
-#include "logicElements.h"
+#include "LogicElements.h"
 #include <unordered_map>
 using json = nlohmann::json;
 inline void to_json(json& j, const Rectangle& rec);
