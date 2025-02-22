ddf6adb9526ce35491ec094729563f9fa3e5ed9d
ecrinyildiz <ecryildiz@gmail.com>
2025-02-22 18:19:15 +0300

    feat: enhance GUI tools with dynamic gate rendering and drag-and-drop functionality

--- include/GUI/GUITools.h
+++ include/GUI/GUITools.h
@@ -1,23 +1,25 @@
 #ifndef GUITOOLS_H
 #define GUITOOLS_H
 #include "LogicElements.h"
+
+// #include <components/TreeView.h>
 namespace GUITools
 {
-	enum class DragDropState
-	{
-		DRAGGING,
-		DROPPED,
-		IDLE,
-	};
-	struct GUIToolsDragDrop
-	{
-		DragDropState state;
-		LogicElements::GateType gateType;
-	};
-	extern GUIToolsDragDrop dragDrop;
-	void GUITools_BasicLogicDisplay();
-	void GUITools_Display();
-}
+    enum class DragDropState
+    {
+        DRAGGING,
+        DROPPED,
+        IDLE,
+    };
+    struct GUIToolsDragDrop
+    {
+        DragDropState state;
+        LogicElements::GateType gateType;
+    };
 
+    extern GUIToolsDragDrop dragDrop;
+    void GUITools_BasicLogicDisplay();
+    void GUITools_Display();
+}  // namespace GUITools
 
-#endif 
+#endif
