7614c6c40abf12a593e6522ff5d7238aaaab841c
ecrinyildiz <ecryildiz@gmail.com>
2025-02-26 00:32:35 +0300

    [+] JsonSerializer: Add save and load functionality for circuits
    - Implement saveCircuit to serialize circuit data to JSON
    - Implement loadCircuit to deserialize circuit data from JSON
    - Introduce to_json functions for LogicGate, Connection, and Vector2
    
    - Enhance circuit management by enabling file-based persistence
    - Improve user experience with easy circuit saving and loading

--- include/JsonSerializer.h
+++ include/JsonSerializer.h
@@ -1,12 +1,43 @@
 #ifndef JSONSERIALIZER_H
 #define JSONSERIALIZER_H
+
 #include "../Libraries/json.hpp"
-#include <string>
 #include "LogicElements.h"
-#include <unordered_map>
-using json = nlohmann::json;
-inline void to_json(json& j, const Rectangle& rec);
-inline void to_json(json& j, const LogicElements::LogicGate& gate);
-inline void to_json(json& j, const CircuitElements::PhysicalConnection& physicalConnection);
-inline void to_json(json& j, const CircuitElements::Connection& connection);
-#endif 
+#include "raylib.h"  // Include Raylib for Vector2
+
+#include <fstream>
+#include <vector>
+
+namespace jsonParser
+{
+    using json = nlohmann::json;
+
+    void saveCircuit(const CircuitElements::Circuit& circuit, const std::string& filePath);
+    std::shared_ptr<CircuitElements::Circuit> loadCircuit(const std::string& filePath);
+
+    // Explicitly declare to_json functions
+    void to_json(json& j, const LogicElements::LogicGate& gate);
+    void to_json(json& j, const CircuitElements::Connection& conn);
+
+}  // namespace jsonParser
+
+// Define to_json for Raylib's Vector2 in nlohmann/json's namespace
+namespace nlohmann
+{
+    template<>
+    struct adl_serializer<Vector2>
+    {
+        static void to_json(json& j, const Vector2& v)
+        {
+            j = json{{"x", v.x}, {"y", v.y}};
+        }
+
+        static void from_json(const json& j, Vector2& v)
+        {
+            j.at("x").get_to(v.x);
+            j.at("y").get_to(v.y);
+        }
+    };
+}  // namespace nlohmann
+
+#endif  // JSONSERIALIZER_H
