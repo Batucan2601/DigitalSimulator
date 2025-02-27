f883ed4057925efec764952571540fa89edca50d
ecrinyildiz <ecryildiz@gmail.com>
2025-02-27 21:17:16 +0300

    feat: add theme support with dark and light modes in GUIStyle

--- include/GUI/GUIStyle.h
+++ include/GUI/GUIStyle.h
@@ -1,9 +1,13 @@
 #ifndef GUI_STYLE_H
 #define GUI_STYLE_H
+#include "appSettings.h"
+
 #include <imgui.h>
 namespace GUIStyle
 {
-	void init();
-	extern ImFont* font;
-}
-#endif 
+    void init();
+    extern ImFont* font;
+    void ApplyDarkTheme();
+    void ApplyLightTheme();
+}  // namespace GUIStyle
+#endif
