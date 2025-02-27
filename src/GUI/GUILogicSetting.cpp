331ae8418addc25f0170df76a7ce14ee83d59e0b
Batucan2601 <tosyalibatuhan@gmail.com>
2025-02-27 17:21:12 +0300

    Input output settings added

--- src/GUI/GUILogicSetting.cpp
+++ src/GUI/GUILogicSetting.cpp
@@ -2,6 +2,7 @@
 #include "Component.h"
 #include "LogicElements.h"
 #include <imgui.h>
+#include <misc/cpp/imgui_stdlib.h>
 #include <raylib.h>
 
 namespace GUILogicSetting
@@ -16,12 +17,46 @@ namespace GUILogicSetting
             GUITools_BasicLogicDisplay_draw(d1);
         }
     }
+    static void draw_Inputs(LogicElements::LogicGate* logicGate)
+    {
+        if (ImGui::TreeNodeEx("Inputs"))
+        {
+            int i = 1;
+            for (auto& inputs : logicGate->outputs)
+            {
+                // Create a unique identifier that doesn't show in the UI.
+                std::string label = "##xx" + std::to_string(i);
+                // Directly pass the reference to the string.
+                ImGui::InputText(label.c_str(), &inputs.name);
+                i++;
+            }
+            ImGui::TreePop();
+        }
+    }
+    static void draw_Outputs(LogicElements::LogicGate* logicGate)
+    {
+        if (ImGui::TreeNodeEx("Outputs"))
+        {
+            int i = 1;
+            for (auto& inputs : logicGate->inputs)
+            {
+                // Create a unique identifier that doesn't show in the UI.
+                std::string label = "##xx" + std::to_string(i);
+                // Directly pass the reference to the string.
+                ImGui::InputText(label.c_str(), &inputs.name);
+                i++;
+            }
+            ImGui::TreePop();
+        }
+    }
     static void GUITools_BasicLogicDisplay_draw(LogicElements::LogicGate* logicGate)
     {
         // Draw interactive windows first.
         float pos[2] = { logicGate->bd.x, logicGate->bd.y };
         ImGui::Begin("Logic Settings", &is_shown);
         ImGui::InputFloat2("Position", pos);
+        draw_Inputs(logicGate);
+        draw_Outputs(logicGate);
         ImGui::End();
 
         logicGate->bd.x = pos[0];
